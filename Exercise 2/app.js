459fe91f50b5d8ae8ac9610975aa496b3a8759b2
Rafael Muto <r.nagahama@gmail.com>
2019-09-06 00:47:26 -0300

    #2 exer done...

--- Exercise 2/app.js
+++ Exercise 2/app.js
@@ -1,6 +1,16 @@
 new Vue({
-        el: '#exercise',
-        data: {
-            value: ''
-        }
-    });
+  el: '#exercise',
+  data: {
+    value: ''
+  },
+  methods: {
+    alertMe: () => {
+      alert('danger danger danger!!!');
+    },
+    type: event => {
+      this.value = event.target.value;
+      console.log(value);
+      return;
+    }
+  }
+});
