b4d6e88c41e73ae8ce190d1e94712840ebed1b54
Rafael Muto <r.nagahama@gmail.com>
2019-09-06 01:01:47 -0300

    ;)

--- Exercise 2/app.js
+++ Exercise 2/app.js
@@ -7,10 +7,11 @@ new Vue({
     alertMe: () => {
       alert('danger danger danger!!!');
     },
-    type: event => {
+    // this will not work with arrow functions!!!
+    // in ES6 arrow function the this. refers to the parrent object wich holds the object, in this case the window object... ;)
+    type: function(event) {
+      console.log(this.value);
       this.value = event.target.value;
-      console.log(value);
-      return;
     }
   }
 });
