314953215ec78b46eaefa7baeff220f3c893ff08
Rafael Muto <r.nagahama@gmail.com>
2019-09-08 23:07:10 -0300

    exer 3

--- Exercise 3/app.js
+++ Exercise 3/app.js
@@ -1,6 +1,19 @@
 new Vue({
-        el: '#exercise',
-        data: {
-            value: 0
-        }
-    });
+  el: '#exercise',
+  data: {
+    value: 0
+  },
+  computed: {
+    result: function() {
+      return this.value >= 37 ? 'done' : 'not there yet';
+    }
+  },
+  watch: {
+    result: function() {
+      let vm = this;
+      setTimeout(function() {
+        vm.value = 0;
+      }, 2000);
+    }
+  }
+});
