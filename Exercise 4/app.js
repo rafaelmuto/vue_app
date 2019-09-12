2e22261db29c5f7cc73eedd9c55036d4b037c126
Rafael Muto <r.nagahama@gmail.com>
2019-09-11 23:18:49 -0300

    exer3

--- Exercise 4/app.js
+++ Exercise 4/app.js
@@ -1,11 +1,38 @@
 new Vue({
   el: '#exercise',
   data: {
-
+    effectClasses: {
+      highlight: false,
+      shrink: true
+    },
+    floatClass: 'float',
+    userClass: '',
+    isVisible: true,
+    myStyle: {
+      width: '100px',
+      height: '150px',
+      backgroundColor: 'gray'
+    },
+    progressBar: {
+      width: '0px',
+      backgroundColor: 'red'
+    }
   },
   methods: {
     startEffect: function() {
-    
+      let vm = this;
+      setInterval(function() {
+        vm.effectClasses.highlight = !vm.effectClasses.highlight;
+        vm.effectClasses.shrink = !vm.effectClasses.shrink;
+      }, 1000);
+    },
+    startProgress: function() {
+      let vm = this;
+      let width = 0;
+      setInterval(function() {
+        width = width + 10;
+        vm.progressBar.width = width + 'px';
+      }, 500);
     }
   }
 });
