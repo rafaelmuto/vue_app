b9e09f58210810ad81dcbf25ac0da03641f312d3
Rafael Muto <r.nagahama@gmail.com>
2019-09-14 01:24:06 -0300

    exer 5 solve

--- Exercise 5/app.js
+++ Exercise 5/app.js
@@ -8,9 +8,10 @@ new Vue({
       books: '3'
     },
     testData: {
-      name: 'TESTOBJECT', 
+      name: 'TESTOBJECT',
       id: 10,
       data: [1.67, 1.33, 0.98, 2.21]
-    }
+    },
+    show: false
   }
 });
