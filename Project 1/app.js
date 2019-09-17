1c2b62dd04fccb29519e01f53dd9a8f6899e90c6
Rafael Muto <r.nagahama@gmail.com>
2019-09-17 00:40:42 -0300

    first proj commit #1

---
+++ Project 1/app.js
@@ -0,0 +1,40 @@
+new Vue({
+  el: '#app',
+  data: {
+    playerHealth: 100,
+    monsterHealth: 100,
+    gameIsRunning: false
+  },
+  methods: {
+    startGame: function() {
+      this.gameIsRunning = true;
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+    },
+    attack: function() {
+      let max = 10;
+      let min = 3;
+      let damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+      this.monsterHealth -= damage;
+
+      if (this.monsterHealth <= 0) {
+        alert('you won!');
+        this.gameIsRunning = false;
+        return;
+      }
+
+      max = 12;
+      min = 5;
+      damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+      this.playerHealth -= damage;
+
+      if (this.playerHealth <= 0) {
+        alert('you lost!');
+        this.gameIsRunning = false;
+      }
+    },
+    specialAttack: function() {},
+    heal: function() {},
+    giveUp: function() {}
+  }
+});
