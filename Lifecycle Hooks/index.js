9049a4bf4e6b34ff11c16363acec8ae5f9c53d54
Rafael Muto <r.nagahama@gmail.com>
2019-09-29 23:44:29 -0300

    filecycle hooks

---
+++ Lifecycle Hooks/index.js
@@ -0,0 +1,35 @@
+new Vue({
+  el: '#app',
+  data: {
+    title: 'The VueJS Instance'
+  },
+  beforeCreate: function() {
+    console.log('beforeCreate()');
+  },
+  created: function() {
+    console.log('created()');
+  },
+  beforeMount: function() {
+    console.log('beforeMount()');
+  },
+  mounted: function() {
+    console.log('mounted()');
+  },
+  beforeCreated: function() {
+    console.log('beforeCreated()');
+  },
+  beforeUpdate: function() {
+    console.log('beforeUpdated()');
+  },
+  beforeDestroy: function() {
+    console.log('beforeDestroy()');
+  },
+  destroyed: function() {
+    console.log('destroyed()');
+  },
+  methods: {
+    destroy: function() {
+      this.$destroy();
+    }
+  }
+});
